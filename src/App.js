da86612969cb3bb872a56facde3edcf9ff84c0ae
Surya <86565276+Surya241200@users.noreply.github.com>
2023-09-10 01:27:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,24 +44,24 @@ function App() {
     const len1 = state.length
     let passwordStr = ""
     if (len1 < 1) toast('Please Enter Password Length')
-    if (!state.upper && !state.lower && !state.number && !state.symbol) toast('Please select alteast one combination')
+    else if (len1.includes('.')) toast('Please Enter Valid Password Length')
+    else if (!state.upper && !state.lower && !state.number && !state.symbol) toast('Please select alteast one combination')
+    else {
+      for (let i = 0; i < len1; i++) {
+        const x = getValue()
+        passwordStr += x
+      }
+      if (passwordStr) {
+        let localPasswords = [...prePassowrds]
+        if (localPasswords.length > 4) localPasswords.pop()
 
-    for (let i = 0; i < len1; i++) {
-      const x = getValue()
-      passwordStr += x
+        if (password) localPasswords.unshift(password)
+        localStorage.setItem('passwords', JSON.stringify(localPasswords))
+        setPassword(passwordStr)
+        setPrePassword(localPasswords)
+      }
+
     }
-
-
-    if (passwordStr) {
-      let localPasswords = [...prePassowrds]
-      if (localPasswords.length > 4) localPasswords.pop()
-
-      if (password) localPasswords.unshift(password)
-      localStorage.setItem('passwords', JSON.stringify(localPasswords))
-      setPassword(passwordStr)
-      setPrePassword(localPasswords)
-    }
-
   }
   const handlePassword = () => {
     navigator.clipboard.writeText(password)
@@ -77,6 +77,8 @@ function App() {
     });
   }
 
+ 
+
   return (
     <div className="App">
       <div>
@@ -88,8 +90,8 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                 <path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z" />
               </svg>
-            </span> }
-          </div> 
+            </span>}
+          </div>
           <div className="body">
             <div className="body-parts">
               <label for="length" id="pw-length">Password Length</label>
